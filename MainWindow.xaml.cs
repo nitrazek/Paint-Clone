28b9ddb348abcd16e9d15e649907b477f21faa2f
Pabu24 <pawel.budnik.1920@gmail.com>
2024-10-10 17:02:25 +0200

    Freehand drawing, improved MainWindow UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,9 +13,35 @@ namespace Paint_Clone
 {
     public partial class MainWindow : Window
     {
+        Point currentPoint = new Point();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void paintSurface_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState != MouseButtonState.Pressed) return;
+            
+            currentPoint = e.GetPosition(paintSurface);
+        }
+
+        private void paintSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+            
+            Line line = new();
+
+            line.Stroke = SystemColors.WindowFrameBrush;
+            line.X1 = currentPoint.X;
+            line.Y1 = currentPoint.Y;
+            line.X2 = e.GetPosition(paintSurface).X;
+            line.Y2 = e.GetPosition(paintSurface).Y;
+
+            currentPoint = e.GetPosition(paintSurface);
+
+            paintSurface.Children.Add(line);
         }
     }
 }
--- viewmodels/MainWindowViewModel.cs
+++ viewmodels/MainWindowViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace Paint_Clone.viewmodels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
-       
+        public ObservableCollection<Shape> Shapes { get; set; } = new ObservableCollection<Shape>();
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private void AddText()
+        {
+
+        }
+
+        private void SaveImage()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = ""
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
